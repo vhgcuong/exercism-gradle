49d1cda2dc1bbc714f17a458603b663b57e68169
yotoko <vhgcuong95@gmail.com>
2026-01-13 21:44:20 +0700

    Annalyns Infiltration

--- app/src/main/java/basic/exercism/annalynsinfiltration/AnnalynsInfiltration.java
+++ app/src/main/java/basic/exercism/annalynsinfiltration/AnnalynsInfiltration.java
@@ -1,5 +1,27 @@
 package basic.exercism.annalynsinfiltration;
 
 public class AnnalynsInfiltration {
-    
+    public static boolean canFastAttack(boolean knightIsAwake) {
+        return !knightIsAwake;
+    }
+
+    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
+    }
+
+    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+        return !archerIsAwake && prisonerIsAwake;
+    }
+
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
+        if (!knightIsAwake && !archerIsAwake && prisonerIsAwake) {
+            return true;
+        }
+
+        if (!archerIsAwake && petDogIsPresent) {
+            return true;
+        }
+        
+        return false;
+    }
 }
