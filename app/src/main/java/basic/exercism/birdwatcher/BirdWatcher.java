239e7426ab7674b591775968c83e1c3514a09579
yotoko <vhgcuong95@gmail.com>
2026-01-13 22:56:46 +0700

    bird watcher stream 2 and remove this when propety is private

--- app/src/main/java/basic/exercism/birdwatcher/BirdWatcher.java
+++ app/src/main/java/basic/exercism/birdwatcher/BirdWatcher.java
@@ -10,33 +10,31 @@ class BirdWatcher {
     }
 
     public int[] getLastWeek() {
-        return this.birdsPerDay;
+        return birdsPerDay;
     }
 
     public int getToday() {
-        return this.birdsPerDay[this.birdsPerDay.length - 1];
+        return birdsPerDay[birdsPerDay.length - 1];
     }
 
     public void incrementTodaysCount() {
-        this.birdsPerDay[this.birdsPerDay.length - 1] = this.getToday() + 1;
+        birdsPerDay[birdsPerDay.length - 1] = this.getToday() + 1;
     }
 
     public boolean hasDayWithoutBirds() {
-        return Arrays.stream(this.birdsPerDay).anyMatch(item -> item == 0);
+        return Arrays.stream(birdsPerDay).anyMatch(item -> item == 0);
     }
 
     public int getCountForFirstDays(int numberOfDays) {
-        int length = this.birdsPerDay.length < numberOfDays 
-            ? this.birdsPerDay.length 
-            : numberOfDays;
         return Arrays
-            .stream(this.birdsPerDay, 0, length)
+            .stream(birdsPerDay)
+            .limit(numberOfDays)
             .sum();
     }
 
     public int getBusyDays() {
         return (int)Arrays
-            .stream(this.birdsPerDay)
+            .stream(birdsPerDay)
             .filter(i -> i >= 5)
             .count();
     }
