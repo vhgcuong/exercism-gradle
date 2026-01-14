3a40ba84cb09549a3d8085d7b0c8007f0a90d8d4
yotoko <vhgcuong95@gmail.com>
2026-01-14 23:17:25 +0700

    Base Calculator Conundrum

---
+++ app/src/main/java/basic/exercism/calculatorconundrum/CalculatorConundrum.java
@@ -0,0 +1,7 @@
+package basic.exercism.calculatorconundrum;
+
+class CalculatorConundrum {
+    public String calculate(int operand1, int operand2, String operation) {
+        throw new UnsupportedOperationException("Please implement the CalculatorConundrum.calculate() method");
+    }
+}
---
+++ app/src/main/java/basic/exercism/calculatorconundrum/IllegalOperationException.java
@@ -0,0 +1,12 @@
+package basic.exercism.calculatorconundrum;
+
+public class IllegalOperationException extends RuntimeException {
+    public IllegalOperationException(String errorMessage) {
+        super(errorMessage);
+    }
+
+    public IllegalOperationException(String errorMessage, Throwable cause) {
+        super(errorMessage, cause);
+    }
+}
+
