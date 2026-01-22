64a3dfb434dc0b4bc696e7f8ba1f77879a6dbae6
yotoko <vhgcuong95@gmail.com>
2026-01-22 23:21:16 +0700

    calculator conundrum

--- app/src/main/java/basic/exercism/calculatorconundrum/CalculatorConundrum.java
+++ app/src/main/java/basic/exercism/calculatorconundrum/CalculatorConundrum.java
@@ -1,7 +1,31 @@
 package basic.exercism.calculatorconundrum;
 
 class CalculatorConundrum {
+    
     public String calculate(int operand1, int operand2, String operation) {
-        throw new UnsupportedOperationException("Please implement the CalculatorConundrum.calculate() method");
+        if (operation == null) {
+            throw new IllegalArgumentException("Operation cannot be null");
+        }
+
+        if (operation == "") {
+            throw new IllegalArgumentException("Operation cannot be empty");
+        }
+
+        try {
+            final int result = switch (operation) {
+                case "+" -> operand1 + operand2;
+                case "*" -> operand1 * operand2;
+                case "/" -> operand1 / operand2;
+                default ->
+                    throw new IllegalOperationException(String.format("Operation '%s' does not exist", operation));
+            };
+            
+            return String.format("%d %s %d = %d", operand1, operation, operand2, result).toString();
+        } catch (ArithmeticException e) {
+            throw new IllegalOperationException(
+                "Division by zero is not allowed",
+                e
+            );
+        }
     }
 }
