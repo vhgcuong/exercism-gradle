8a873168d7ff39e3b6975d9b4d2f5323f87594fc
yotoko <vhgcuong95@gmail.com>
2026-01-22 23:26:04 +0700

    Calculator Conundrum 1

--- app/src/main/java/basic/exercism/calculatorconundrum/CalculatorConundrum.java
+++ app/src/main/java/basic/exercism/calculatorconundrum/CalculatorConundrum.java
@@ -7,25 +7,19 @@ class CalculatorConundrum {
             throw new IllegalArgumentException("Operation cannot be null");
         }
 
-        if (operation == "") {
-            throw new IllegalArgumentException("Operation cannot be empty");
-        }
-
         try {
             final int result = switch (operation) {
                 case "+" -> operand1 + operand2;
                 case "*" -> operand1 * operand2;
                 case "/" -> operand1 / operand2;
+                case "" -> throw new IllegalArgumentException("Operation cannot be empty");
                 default ->
                     throw new IllegalOperationException(String.format("Operation '%s' does not exist", operation));
             };
-            
+
             return String.format("%d %s %d = %d", operand1, operation, operand2, result).toString();
         } catch (ArithmeticException e) {
-            throw new IllegalOperationException(
-                "Division by zero is not allowed",
-                e
-            );
+            throw new IllegalOperationException("Division by zero is not allowed", e);
         }
     }
 }
