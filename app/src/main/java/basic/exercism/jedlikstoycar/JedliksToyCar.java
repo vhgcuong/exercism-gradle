8ffe0e309a160709c5e3ffdb1542f17f5ce91e1f
yotoko <vhgcuong95@gmail.com>
2026-01-22 23:30:38 +0700

    Base JedliksToyCar

---
+++ app/src/main/java/basic/exercism/jedlikstoycar/JedliksToyCar.java
@@ -0,0 +1,19 @@
+package basic.exercism.jedlikstoycar;
+
+public class JedliksToyCar {
+    public static JedliksToyCar buy() {
+        throw new UnsupportedOperationException("Please implement the (static) JedliksToyCar.buy()  method");
+    }
+
+    public String distanceDisplay() {
+        throw new UnsupportedOperationException("Please implement the JedliksToyCar.distanceDisplay()  method");
+    }
+
+    public String batteryDisplay() {
+        throw new UnsupportedOperationException("Please implement the JedliksToyCar.batteryDisplay()  method");
+    }
+
+    public void drive() {
+        throw new UnsupportedOperationException("Please implement the JedliksToyCar.drive()  method");
+    }
+}
