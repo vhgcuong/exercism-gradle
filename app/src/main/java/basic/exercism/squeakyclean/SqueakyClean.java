2419f3df589ddb2262b2a3f43d67ff6dfb21cf24
yotoko <vhgcuong95@gmail.com>
2026-01-29 23:44:57 +0700

    Base squeaky clean

---
+++ app/src/main/java/basic/exercism/squeakyclean/SqueakyClean.java
@@ -0,0 +1,8 @@
+package basic.exercism.squeakyclean;
+
+class SqueakyClean {
+    static String clean(String identifier) {
+        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+    }
+}
+
