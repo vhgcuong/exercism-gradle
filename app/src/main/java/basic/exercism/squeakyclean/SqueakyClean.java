8e8aa2c82765de66a14dc97cdba76e50c06029e5
yotoko <vhgcuong95@gmail.com>
2026-02-05 22:51:29 +0700

    Squeaky Clean 2

--- app/src/main/java/basic/exercism/squeakyclean/SqueakyClean.java
+++ app/src/main/java/basic/exercism/squeakyclean/SqueakyClean.java
@@ -1,26 +1,16 @@
 package basic.exercism.squeakyclean;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SqueakyClean {
     public static String clean(String identifier) {
-        String step1 = identifier.replaceAll("\\s", "_");
-        Matcher matcher = Pattern.compile("-(\\w)").matcher(step1);
-        StringBuffer sb = new StringBuffer();
-        while (matcher.find()) {
-            matcher.appendReplacement(sb, matcher.group(1).toUpperCase());
-        }
-        matcher.appendTail(sb);
-        String step2 = sb.toString();
-        String step3 = step2.replaceAll("4", "a")
-                            .replaceAll("3", "e")
-                            .replaceAll("0", "o")
-                            .replaceAll("1", "l")
-                            .replaceAll("7", "t");
-        String step4 = step3.replaceAll("[^a-zA-Z_]", "");
-        
-        return step4;
+        return Pattern.compile("-(\\p{IsAlphabetic})")
+            .matcher(identifier)
+            .replaceAll(m -> m.group().toUpperCase())
+            .replaceAll("\s", "_")
+            .replaceAll("\\p{Cntrl}", "CTRL")
+            .replaceAll("[^_\\p{IsAlphabetic}]", "")
+            .replaceAll("[α-ω]", "");
     }
 }
 
