61e453477368e88e82443e68bef3742011d79fa6
yotoko <vhgcuong95@gmail.com>
2026-03-15 23:00:34 +0700

    cars assemble refacter

--- app/src/main/java/basic/exercism/carsassemble/CarsAssemble.java
+++ app/src/main/java/basic/exercism/carsassemble/CarsAssemble.java
@@ -3,19 +3,21 @@ package basic.exercism.carsassemble;
 public class CarsAssemble {
     private final static int CARS_EACH_HOUR = 221;
     
-    public double productionRatePerHour(int speed) {
-        final double rate = switch (speed) {
-            case 1, 2, 3, 4 -> 1;
+    public double productionRatePerHour(final int speed) {
+        return successRate(speed) * speed * CARS_EACH_HOUR;
+    }
+
+    public int workingItemsPerMinute(final int speed) {
+        return (int) productionRatePerHour(speed) / 60;
+    }
+
+    private double successRate(final int speed) {
+        return switch (speed) {
+            case 1, 2, 3, 4 -> 1.0;
             case 5, 6, 7, 8 -> 0.9;
             case 9 -> 0.8;
             case 10 -> 0.77;
-            default -> 0;
+            default -> 0.0;
         };
-
-        return rate * speed * CARS_EACH_HOUR;
-    }
-
-    public int workingItemsPerMinute(int speed) {
-        return (int) this.productionRatePerHour(speed) / 60;
     }
 }
