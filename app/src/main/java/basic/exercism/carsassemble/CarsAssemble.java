35e824368265917f55dd7db1fabefd4138fd1526
yotoko <vhgcuong95@gmail.com>
2026-03-15 23:02:48 +0700

    Cars assemble inline method + optimize switch

--- app/src/main/java/basic/exercism/carsassemble/CarsAssemble.java
+++ app/src/main/java/basic/exercism/carsassemble/CarsAssemble.java
@@ -1,23 +1,19 @@
 package basic.exercism.carsassemble;
 
 public class CarsAssemble {
-    private final static int CARS_EACH_HOUR = 221;
-    
-    public double productionRatePerHour(final int speed) {
-        return successRate(speed) * speed * CARS_EACH_HOUR;
-    }
+    private static final int CARS_EACH_HOUR = 221;
 
-    public int workingItemsPerMinute(final int speed) {
-        return (int) productionRatePerHour(speed) / 60;
-    }
-
-    private double successRate(final int speed) {
+    public double productionRatePerHour(int speed) {
         return switch (speed) {
-            case 1, 2, 3, 4 -> 1.0;
-            case 5, 6, 7, 8 -> 0.9;
-            case 9 -> 0.8;
-            case 10 -> 0.77;
-            default -> 0.0;
+            case 1,2,3,4 -> speed * CARS_EACH_HOUR;
+            case 5,6,7,8 -> speed * CARS_EACH_HOUR * 0.9;
+            case 9 -> speed * CARS_EACH_HOUR * 0.8;
+            case 10 -> speed * CARS_EACH_HOUR * 0.77;
+            default -> 0;
         };
+    }
+
+    public int workingItemsPerMinute(int speed) {
+        return (int)(productionRatePerHour(speed) / 60);
     }
 }
