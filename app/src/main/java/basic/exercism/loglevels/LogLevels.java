efbf15e323279dd31ffba0f37054de54b2fa90d4
yotoko <vhgcuong95@gmail.com>
2026-03-29 22:55:12 +0700

    log level using format

--- app/src/main/java/basic/exercism/loglevels/LogLevels.java
+++ app/src/main/java/basic/exercism/loglevels/LogLevels.java
@@ -1,31 +1,18 @@
 package basic.exercism.loglevels;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import static java.lang.String.format;
 
 public class LogLevels {
     
     public static String message(String logLine) {
-        Matcher m = Pattern.compile("\\[(\\w+)]\\:(.+)").matcher(logLine);
-        if (m.find()) {
-            return m.group(2).trim();
-        }
-        return "";
+        return logLine.split("]:")[1].trim();
     }
 
     public static String logLevel(String logLine) {
-        Matcher m = Pattern.compile("\\[(\\w+)]\\:(.+)").matcher(logLine);
-        if (m.find()) {
-            return m.group(1).toLowerCase();
-        }
-        return "";
+        return logLine.split("]:")[0].substring(1).toLowerCase();
     }
 
     public static String reformat(String logLine) {
-        Matcher m = Pattern.compile("\\[(\\w+)]\\:(.+)").matcher(logLine);
-        if (m.find()) {
-            return m.group(2).trim() + " (" + m.group(1).toLowerCase() + ")";
-        }
-        return "";
+        return format("%s (%s)", message(logLine), logLevel(logLine));
     }
 }
