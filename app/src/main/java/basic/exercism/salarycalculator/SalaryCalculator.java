1811de6605fdca2388e41f06c8d7ec247c93674e
yotoko <vhgcuong95@gmail.com>
2026-03-29 23:01:26 +0700

    Salary Calculator

---
+++ app/src/main/java/basic/exercism/salarycalculator/SalaryCalculator.java
@@ -0,0 +1,20 @@
+package basic.exercism.salarycalculator;
+
+
+public class SalaryCalculator {
+    public double salaryMultiplier(int daysSkipped) {
+        throw new UnsupportedOperationException("Please implement the SalaryCalculator.salaryMultiplier() method");
+    }
+
+    public int bonusMultiplier(int productsSold) {
+        throw new UnsupportedOperationException("Please implement the SalaryCalculator.bonusMultiplier() method");
+    }
+
+    public double bonusForProductsSold(int productsSold) {
+        throw new UnsupportedOperationException("Please implement the SalaryCalculator.bonusForProductsSold() method");
+    }
+
+    public double finalSalary(int daysSkipped, int productsSold) {
+        throw new UnsupportedOperationException("Please implement the SalaryCalculator.finalSalary() method");
+    } 
+}
